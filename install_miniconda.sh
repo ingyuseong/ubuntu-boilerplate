ad39586342996edf2e6586f662bd177b7eb28abd
ingyuseong <dkc984@likelion.org>
2022-04-21 08:15:37 +0000

    Update miniconda installation

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -44,15 +44,4 @@ $HOME/miniconda3/bin/conda init
 bash
 source activate base
 which python
-source activate base
-source activate base
-source activate base
-source activate base
-source activate base
-source activate base
-source activate base
-source activate base
-which python
-source activate base
-which python
-source activate base
+
